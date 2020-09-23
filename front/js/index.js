8f53a080e56f00c4c7ee50641a3853c19eb198d3
Marie Paret <marieparet.33@gmail.com>
2020-09-23 15:32:41 +0200

    improve responsive design and add fonts

--- front/js/index.js
+++ front/js/index.js
@@ -11,12 +11,15 @@ fetch(BASE_URL + "api/teddies/")
         <div class="card">
           <span>
           <a href="produit.html?id=${product._id}" class="text-dark">
-          <img class="card-img-top" src="${product.imageUrl}" alt=".">
+          <img class="card-img-top" src="${
+            product.imageUrl
+          }" alt="Photo d'ours en peluche">
           </span>
           
           <div class="card-body">
             <h2 class="card-title">${product.name}</h2>
             <p class="card-text">${product.price / 100} €</p>
+            <p class="card-text">(${product.colors.length} coloris)</p>
           </div>
         </div>
         </a>
