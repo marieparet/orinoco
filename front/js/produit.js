a87518d63c40c931d1d4f42edead00d134d884b0
Marie Paret <marieparet.33@gmail.com>
2020-09-23 12:40:34 +0200

    continue product page

--- front/js/produit.js
+++ front/js/produit.js
@@ -5,6 +5,7 @@ function getIdFromUrl() {
   return idNumber.substr(4); ///on supprime les 4 1ers caractères de la string ("?=id")
 }
 
+//requête http vers l'élément correspondant à l'id donné
 fetch(BASE_URL + `api/teddies/${getIdFromUrl()}`)
   .then((response) => response.json())
   .then((product) => {
@@ -13,5 +14,26 @@ fetch(BASE_URL + `api/teddies/${getIdFromUrl()}`)
       .querySelector(".product-img")
       .setAttribute("src", product.imageUrl);
     document.querySelector(".card-title").innerHTML = product.name;
+    document.querySelector(".card-price").innerHTML = `Prix : ${
+      product.price / 100
+    } €`;
+    document.querySelector(".card-description").innerHTML = product.description;
+
+    //création des options couleurs pour le formulaire de personnalisation
+    let select = document.getElementById("FormControlSelect");
+
+    product.colors.forEach((color) => {
+      let option = document.createElement("option");
+      option.value = color;
+      option.innerText = color;
+      select.appendChild(option);
+    });
   })
   .catch((error) => console.error(error));
+
+//redirection vers la page panier au clic sur le bouton "Ajouter au panier"
+let button = document.querySelector("#add-to-cart");
+
+button.addEventListener("click", function () {
+  window.location.href = "panier.html";
+});
