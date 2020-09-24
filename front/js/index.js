3494b1adac683f8c38cbccdc4f58140277bdc461
Marie Paret <marieparet.33@gmail.com>
2020-09-24 11:01:19 +0200

    adding functions to fetch requests

--- front/js/index.js
+++ front/js/index.js
@@ -1,30 +1,36 @@
 const BASE_URL = "http://localhost:3000/";
 
-fetch(BASE_URL + "api/teddies/")
-  .then((response) => response.json()) //on formate les données en .json
-  .then((products) => {
-    //on récupère les données
-    products.forEach((product) => {
-      console.log(product);
-      document.querySelector("#products").innerHTML += `
-      <div class="col-12 col-lg-4">
-        <div class="card">
-          <span>
-          <a href="produit.html?id=${product._id}" class="text-dark">
-          <img class="card-img-top" src="${
-            product.imageUrl
-          }" alt="Photo d'ours en peluche">
-          </span>
-          
-          <div class="card-body">
-            <h2 class="card-title">${product.name}</h2>
-            <p class="card-text">${product.price / 100} €</p>
-            <p class="card-text">(${product.colors.length} coloris)</p>
-          </div>
-        </div>
-        </a>
+function fetchTeddies() {
+  fetch(BASE_URL + "api/teddies/")
+    .then((response) => response.json())
+    .then((products) => processEachTeddy(products))
+    .catch((error) => console.error(error));
+}
+
+function insertTeddyInList(product) {
+  let productDiv = document.querySelector("#products");
+  productDiv.innerHTML += `
+  <div class="col-12 col-lg-4">
+    <div class="card shadow-sm">
+      <span id="image-wrapper">
+      <a href="produit.html?id=${product._id}" class="text-dark">
+      <img class="card-img-top" src="${product.imageUrl}"
+      alt="Photo d'ours en peluche">
+      </span>
+      
+      <div class="card-body">
+        <h2 class="card-title">${product.name}</h2>
+        <p class="card-text">${product.price / 100} €</p>
+        <p class="card-text">(${product.colors.length} coloris)</p>
       </div>
-      `;
-    });
-  })
-  .catch((error) => console.error(error));
+    </div>
+    </a>
+  </div>
+  `;
+}
+
+function processEachTeddy(products) {
+  products.forEach((product) => insertTeddyInList(product));
+}
+
+fetchTeddies();
--- front/js/produit.js
+++ front/js/produit.js
@@ -1,39 +1,60 @@
 const BASE_URL = "http://localhost:3000/";
 
-function getIdFromUrl() {
-  let idNumber = window.location.search; //on récupère le param d'url que l'on va passer dans la page produit
-  return idNumber.substr(4); ///on supprime les 4 1ers caractères de la string ("?=id")
+//récupération de l'id du produit depuis la query string
+function getProductIdFromUrl() {
+  const queryString = window.location.search;
+  const productId = queryString.substr(4); ///on supprime les 4 1ers caractères de la string ("?=id")
+  return productId;
+}
+
+//insertion des info du teddy passé en paramètre
+function processTeddyInfo(product) {
+  let productImage = document.querySelector(".product-img");
+  let productName = document.querySelector(".card-title");
+  let productPrice = document.querySelector(".card-price");
+  let productDescription = document.querySelector(".card-description");
+
+  productImage.setAttribute("src", product.imageUrl);
+  productName.innerHTML = product.name;
+  productPrice.innerHTML = `Prix : ${product.price / 100} €`;
+  productDescription.innerHTML = product.description;
+}
+
+//création des options couleurs pour le formulaire de personnalisation
+function setColorsOptions(product) {
+  let select = document.getElementById("FormControlSelect");
+
+  product.colors.forEach((color) => {
+    let option = document.createElement("option");
+    option.value = color;
+    option.innerText = color;
+    select.appendChild(option);
+  });
 }
 
 //requête http vers l'élément correspondant à l'id donné
-fetch(BASE_URL + `api/teddies/${getIdFromUrl()}`)
+fetch(BASE_URL + `api/teddies/${getProductIdFromUrl()}`)
   .then((response) => response.json())
   .then((product) => {
-    console.log(product);
-    document
-      .querySelector(".product-img")
-      .setAttribute("src", product.imageUrl);
-    document.querySelector(".card-title").innerHTML = product.name;
-    document.querySelector(".card-price").innerHTML = `Prix : ${
-      product.price / 100
-    } €`;
-    document.querySelector(".card-description").innerHTML = product.description;
-
-    //création des options couleurs pour le formulaire de personnalisation
-    let select = document.getElementById("FormControlSelect");
-
-    product.colors.forEach((color) => {
-      let option = document.createElement("option");
-      option.value = color;
-      option.innerText = color;
-      select.appendChild(option);
-    });
+    processTeddyInfo(product);
+    setColorsOptions(product);
   })
   .catch((error) => console.error(error));
 
 //redirection vers la page panier au clic sur le bouton "Ajouter au panier"
 let button = document.querySelector("#add-to-cart");
 
+//au clic sur le bouton "Ajouter au panier", message d'alerte "Produit ajouté au panier"
 button.addEventListener("click", function () {
-  window.location.href = "panier.html";
+  document.querySelector(".card-body").insertAdjacentHTML(
+    "beforeend",
+    `
+    <div id="added-to-cart" class="alert mt-3 mb-0 alert-success alert-dismissible fade show" role="alert">
+      <p class="alert-heading">Produit ajouté au panier</p>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">×</span>
+      </button>
+    </div>
+    `
+  );
 });
